1c8bb59ae1f086a9684b3e2a6d3b3780a1c473f8
Amit Rathi <amit@hypertrack.io>
2018-05-14 01:36:56 +0530

    Attempt at setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nurtch-docs',
+      version='0.0.3',
+      description='Documentation for the Nurtch platform',
+      url='https://github.com/amit1rrr/nurtch-docs',
+      author='Amit Rathi',
+      author_email='team@nurtch.com',
+      license='MIT',
+      zip_safe=False)
